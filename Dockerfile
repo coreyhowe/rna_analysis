c13656dbe0b0742995ec3d67c4a45bfd139a5934
Corey M Howe <coreyhowe@Corey-M-Howes-MacBook-Pro.local>
2022-05-24 00:10:03 -0600

    added output folder feature

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM 812206152185.dkr.ecr.us-west-2.amazonaws.com/wf-base:fbe8-main
 
 RUN apt-get update -y &&\
-    apt-get install -y wget libncurses5
+    apt-get install -y wget libncurses5 &&\
+    apt-get install gzip
 
 
 ENV PATH="/root/miniconda3/bin:${PATH}"
@@ -17,20 +18,27 @@ RUN wget \
     && rm -f Miniconda3-latest-Linux-x86_64.sh 
 
 
-RUN conda config --add channels defaults
 RUN conda config --add channels bioconda
 RUN conda config --add channels conda-forge
 
 
-RUN conda install -c bioconda fastp trinity transrate trinotate transdecoder blast hmmer
+RUN conda install -c bioconda fastp transrate jellyfish
+#RUN conda install -c bioconda fastp transrate trinotate transdecoder blast hmmer jellyfish
 RUN conda install -c conda-forge sqlite
+#RUN conda install trinity 
+
+RUN curl -L -O https://github.com/trinityrnaseq/trinityrnaseq/releases/download/Trinity-v2.14.0/trinityrnaseq-v2.14.0.FULL.tar.gz &&\
+	#gunzip trinityrnaseq-v2.14.0.FULL.tar.gz &&\
+	tar -zxvf trinityrnaseq-v2.14.0.FULL.tar.gz &&\
+	#make install
+	mv trinityrnaseq-v2.14.0 Trinity
 
 
-
-
+#
 # STOP HERE:
 # The following lines are needed to ensure your build environment works
 # correctly with latch.
+
 COPY wf /root/wf
 ARG tag
 ENV FLYTE_INTERNAL_IMAGE $tag
--- wf/__init__.py
+++ wf/__init__.py
@@ -8,39 +8,12 @@ from pathlib import Path
 from latch import small_task, large_task, workflow
 from latch.types import LatchFile, LatchDir
 
-
-#@small_task
-#def getseq_task(SRR: str) -> (LatchFile, LatchFile):
-
-    # A reference to our output.
- #   read1 = Path("read1.fq.gz").resolve()
-  #  read2 = Path("read2.fq.gz").resolve()
-
-   # _sra-tools1_cmd = [
-    #    "sra-tools",
-     #   "prefetch",
-      #  SRR,
-    #]
-    
-     #_sra-tools2_cmd = [
-      #  "sra-tools",
-       # "fasterq",
-       # SRR,
-        #"--split_files",
-    #]
-
-    #subprocess.run(_sra-tools1_cmd)
-    #subprocess.run(_sra-tools2_cmd)
-
-    #return LatchFile(read1, "latch:///read1.fq.gz")
-    #return LatchFile(read2, "latch:///read2.fq.gz")
-
-
 @small_task
-def preqc_task(read1: LatchFile, read2: LatchFile) -> (LatchFile, LatchFile):
+def preqc_task(read1: LatchFile, read2: LatchFile, output_dir: LatchDir,) -> (LatchFile, LatchFile, LatchDir):
 
-    qc_read1 = Path("qc_read1.fq.gz").resolve()
+    qc_read1 = Path("qc_read1.fq.gz").resolve() 
     qc_read2 = Path("qc_read2.fq.gz").resolve()
+    local_output_dir = "/root/fastp_output/"
 
     _fastp_cmd = [
         "fastp",
@@ -56,115 +29,38 @@ def preqc_task(read1: LatchFile, read2: LatchFile) -> (LatchFile, LatchFile):
 
     subprocess.run(_fastp_cmd)
 
-    return (LatchFile(str(qc_read1), "latch:///qc_read1.fq.gz"), LatchFile(str(qc_read2), "latch:///qc_read2.fq.gz"))
+    return (LatchFile(str(qc_read1), f"latch:///{output_dir}/qc_read1.fq.gz"), LatchFile(str(qc_read2), f"latch:///{output_dir}/qc_read2.fq.gz"), LatchDir(str(local_output_dir), output_dir.remote_path))
     
+
 @large_task
-def assemble_task(qc_read1: LatchFile, qc_read2: LatchFile) -> LatchFile:
+def assemble_task(qc_read1: LatchFile, qc_read2: LatchFile, output_dir: LatchDir) -> (LatchFile, LatchDir):
 # ()
 
-    transcripts = Path("Trinity.fasta").resolve()
+    transcripts = Path(f"trinity_output/Trinity.fasta").resolve()
+    local_output_dir =  "/root/assemble_output/"
 
     _trinity_cmd = [
         "Trinity",
         "--seqType",
         "fq",
         "--left",
-        "latch:///qc_read1.fq.gz",
+        qc_read1.local_path,
         "--right",
-        "latch:///qc_read2.fq.gz",
+        qc_read2.local_path,
         "--max_memory",
         "1000G",
+        "--output",
+        "trinity_output",
+        "--no_bowtie",
     ]
 
     subprocess.run(_trinity_cmd)
 
-    return LatchFile(str(transcripts), "latch:///Trinity.fasta")
+    return (LatchFile(str(transcripts), f"latch:///{output_dir}/trinity_output/Trinity.fasta"), LatchDir(str(local_output_dir), output_dir.remote_path)) 
 
 
-#@large_task
-#def postqc_task(transcripts: LatchFile) -> LatchFile:
-
- #   postqc_report = Path("transrate_results/transrate.csv").resolve()
-
-  #  _transrate_cmd = [
-  #      "transrate",
-   #     "--assembly",
-   #     transcripts.local_path,
-   #     "--left",
-   #     qcread1.local_path,
-   #     "--right",
-    #    qc_read2.local_path,
-    #]
-
-    #subprocess.run(_transrate_cmd)
-
-    #return LatchFile(postqc_report, "latch:///transrate.csv")
-    
-#@large_task
-#def getorf_task(transcripts: LatchFile):
-
- #  _transdecoder1_cmd = [
-  #     "Transdecoder.LongOrfs",
-   #    "-t",
-    #   transcripts.local_path
-   #]
-
-   #subprocess.run(_transdecoder1_cmd)
-    
-#@large_task
-#def getcds_task(transcripts: LatchFile) -> LatchFile:
-
- #  pep = Path("Trinity.Fasta.transdecoder.pep").resolve()
-
-  # _transdecoder2_cmd = [
-   #    "Transdecoder.Predict",
-    #   "-t",
-     #  transcripts.local_path
- # ]
-
-  # subprocess.run(_transdecoder2_cmd)
-
-   #return LatchFile(pep, "latch:///Trinity.Fasta.transdecoder.pep")
-
-#@medium_task
-#def annotate_task(transcripts: LatchFile) -> LatchDir:
-
-	#resultsdb = Path("Trinotate.sqlite").resolve() 
-	#uniprot = Path("uniprot_sprot.pep").resolve() 
-	#pfam =  Path("Pfam-A.hmm.gz").resolve()  
-	#blastdb = 
-
-#_build1_cmd = ["Build_Trinotate_Boilerplate_SQLite_db.pl", "Trinotate"]
-#_build2_cmd = ["makeblastdb", "-in", "uniprot_sprot.pep", "-dbtype", "prot"]
-#_build3_cmd = ["gunzip", "Pfam-A.hmm.gz"]
-#_build4_cmd = ["hmmpress", "Pfam-A.hmm"]
-#_build5_cmd = ["blastp", "-query", "transdecoder.pep", "-db", "uniprot_sprot.pep", "-num_threads", "8", "-max_target_seqs", "1", "-outfmt", "6", "-evalue", "1e-3", ">", "blastp.outfmt6"]
-#_build6_cmd = ["hmmscan", "--cpu", "12", "--domtblout", "TrinotatePFAM.out", "Pfam-A.hmm", "transdecoder.pep", ">", "pfam.log"]	
-
-   # subprocess.run(_build1_cmd)
-   # subprocess.run(_build2_cmd)
-   # subprocess.run(_build3_cmd)
-    #subprocess.run(_build4_cmd)
-    #subprocess.run(_build5_cmd)
-    #subprocess.run(_build6_cmd)
-    
-#@medium_task
-#def results_task(transcripts: LatchFile) -> LatchDir:
-
-#_report1_cmd = ["get_Trinity_gene_to_trans_map.pl", "Trinity.fasta", ">",  "Trinity.fasta.gene_trans_map"]
-#_report2_cmd = ["Trinotate", "Trinotate.sqlite", "init", "--gene_trans_map", "Trinity.fasta.gene_trans_map", "--transcript_fasta", "Trinity.fasta", "--transdecoder_pep", "transdecoder.pep"]
-#_report3_cmd = ["Trinotate", "Trinotate.sqlite", "LOAD_swissprot_blastp", "blastp.outfmt6"]	
-#_report4_cmd = ["Trinotate", "Trinotate.sqlite", "LOAD_pfam", "TrinotatePFAM.out"]
-#_report5_cmd = ["Trinotate", "Trinotate.sqlite", "report", ">", "trinotate_annotation_report.xls"]
-
-    #subprocess.run(_report1_cmd)
-    #subprocess.run(_report2_cmd)
-    #subprocess.run(_report3_cmd)
-    #subprocess.run(_report4_cmd)
-    #subprocess.run(_report5_cmd)
-    
 @workflow
-def assemble_annotate(read1: LatchFile, read2: LatchFile) -> (LatchFile, LatchFile, LatchFile):
+def rna_denovo_assemble(read1: LatchFile, read2: LatchFile, output_dir: LatchDir) -> (LatchFile, LatchDir):
     """
 
     # RNA De Novo Assembly
@@ -194,8 +90,14 @@ def assemble_annotate(read1: LatchFile, read2: LatchFile) -> (LatchFile, LatchFi
 
           __metadata__:
             display_name: Read2
+            
+        output_dir:
+          The directory where results will go.
+          
+          __metadata__:
+            display_name: Output Directory
     """
-    qc_read1, qc_read2 = preqc_task(read1=read1, read2=read2)
-    transcripts = assemble_task(qc_read1=qc_read1, qc_read2=qc_read2)
+    qc_read1, qc_read2, local_output_dir = preqc_task(read1=read1, read2=read2, output_dir=output_dir)
+    transcripts, local_output_dir = assemble_task(qc_read1=qc_read1, qc_read2=qc_read2, output_dir=output_dir)
    # return postqc_task(transcripts=transcripts)
-    return (qc_read1, qc_read2, transcripts)
+    return (transcripts, local_output_dir)
